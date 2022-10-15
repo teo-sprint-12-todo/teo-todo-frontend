5905f8ca2d5d9c863be06d83b1d9988226cf16f4
JongWoo Ha <jongwooha98@gmail.com>
2022-10-15 21:18:50 +0900

    fix: fix compile errors

--- src/components/TodoListElem.tsx
+++ src/components/TodoListElem.tsx
@@ -1,62 +1,61 @@
-import React from 'react'
-import styled from 'styled-components'
-import {Item} from '../pages/MainTodoList'
+import React from 'react';
+import styled from 'styled-components';
+import type { Item } from '../pages/MainTodoList';
 
-interface ContextsProps{
-    item:object;
-}
-
-const ElemHeader = styled.div `
+const ElemHeader = styled.div`
     background-color : black;
     height:inherit;
     width:3%;
     border-radius : 5px 0 0 5px;
-`
+`;
 
-const ElemContent = styled.div `
+const ElemContent = styled.div`
     background-color : #cfcfcf;
     width : 100%;
     display:flex;
     align-items : center;
     padding : 1em
-`
+`;
 
-const Element = styled.div `
+const Element = styled.div`
     display : flex;
     height : 10vh;
-`
+`;
 
-const Content = styled.div `
+const Content = styled.div`
     display : flex;
     width : 100%;
     justify-content :space-between;
-`
+`;
 
-const Check_label = styled.label``
+const CheckLabel = styled.label``;
 
-const Check = styled
-    .input`
+const Check = styled.input`
     width:2em;
-    border: solid black 5px;
-`
+    border: solid black 5px;    
+    `;
 
-export const TodoListElem = (item:Item) => {
-    const {importance, context} = item
+function TodoListElem(item:Item) {
+  const { importance, context } = item;
 
-    return (
-        <Element>
-            <ElemHeader></ElemHeader>
-            <ElemContent>
-                <Content>
-                    <Check_label
-                        htmlFor = "contentCheck"
-                    >{context}</Check_label>
-                    <Check
-                        type = "checkbox"
-                        name = "contentCheck"
-                    ></Check>
-                </Content>
-            </ElemContent>
-        </Element>
-    )
+  return (
+    <Element>
+      <ElemHeader />
+      <ElemContent>
+        <Content>
+          <CheckLabel
+            htmlFor="contentCheck"
+          >
+            {context}
+          </CheckLabel>
+          <Check
+            type="checkbox"
+            name="contentCheck"
+          />
+        </Content>
+      </ElemContent>
+    </Element>
+  );
 }
+
+export default TodoListElem;
