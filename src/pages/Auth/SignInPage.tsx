0a3fdc59ca4da13a4be7484c17126eb72376e913
dev-yun <yunpro2766@gmail.com>
2022-10-17 02:09:33 +0900

    feat: 로그인, 회원가입 기능 구현, Api를 통해 서버와 연동

--- src/pages/Auth/SignInPage.tsx
+++ src/pages/Auth/SignInPage.tsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import AuthForm from '../../components/Auth/AuthForm';
 import AuthTemplate from '../../components/Auth/AuthTemplate';
 
 function SignInPage() {
+  const [userId, setUserId] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+
+  const navigate = useNavigate();
+  const { state } = useLocation();
+
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+
+  const raw = JSON.stringify({
+    email: userId,
+    password: userPassword,
+  });
+
+  const requestOptions: any = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow',
+  };
+
+  const requestSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        'http://3.35.154.223:8080/v1/user/sign-in',
+        requestOptions
+      );
+      const result: any = await response.json();
+      console.log(result);
+
+      localStorage.setItem('user-token', result.data);
+
+      if (localStorage.getItem('user-token') === 'null') {
+        navigate(state);
+      } else {
+        navigate('/mainTodoList');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <AuthTemplate>
-      <AuthForm type="signIn" />
+      <AuthForm
+        setUserId={setUserId}
+        setUserPassword={setUserPassword}
+        requestSign={requestSignIn}
+        type="signIn"
+      />
     </AuthTemplate>
   );
 }
