d4fc4bdb43fca593e5f44693435d7e7fdec5feb2
unknown <director20844@gmail.com>
2022-10-16 14:38:38 +0900

    feat:GoalNav, change List by GoalNav

---
+++ src/components/GoalNav.tsx
@@ -0,0 +1,58 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+const NavBox = styled.div `
+  width:100%;
+  border-block:solid #BCBCC0 1px;
+  padding: 0.7em 0;
+  display:flex;
+  justify-content:space-around;
+`
+
+const NavElem = styled.div <{isProgress:boolean}>`
+  color : ${(props)=>(props.isProgress ? "#1C1C1E": "#8E8E93")} ;
+  display:flex;
+`
+
+const ElemCntProgress = styled.div<{isProgress:boolean}> `
+  color : ${(props)=>(props.isProgress ? "#D11136": "#F7B5BF")} ;
+  padding-left:0.2em;
+`
+
+const ElemCntComplete = styled.div<{isProgress:boolean}> `
+  color : ${(props)=>(props.isProgress ? "#224AC6": "#9EA9E9")} ;
+  padding-left:0.2em;
+`
+
+interface Changed{
+  isComplete:boolean,
+  changeList:(comp:boolean) => void
+}
+
+function GoalNav({isComplete, changeList}:Changed) {
+  const [progress, setProgress] = useState<boolean>(true)
+  const [complete, setComplete] = useState<boolean>(!progress)
+
+  const progressHandler = ():void=>{
+    setProgress(true);
+    setComplete(false);
+    changeList(true);
+  }
+
+  const completeHandler = ():void=>{
+    setProgress(false);
+    setComplete(true);
+    changeList(false);
+  }
+
+  return (
+      <NavBox>
+          <NavElem onClick={progressHandler} isProgress={progress}>진행중
+              <ElemCntProgress isProgress={progress}>5</ElemCntProgress>건</NavElem>
+          <NavElem onClick={completeHandler} isProgress={complete}>종료
+              <ElemCntComplete isProgress={complete}>5</ElemCntComplete>건</NavElem>
+      </NavBox>
+  );
+}
+
+export default GoalNav;
--- src/pages/GoalList/index.tsx
+++ src/pages/GoalList/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import GoalElem from '../../components/GoalElem';
+import GoalNav from '../../components/GoalNav';
 
 export interface Goal {
     "id" : number,
@@ -15,6 +16,7 @@ export interface Goal {
 const GoalListBox = styled.div `
     display:flex;
     flex-direction:column;
+    padding: 0 1.2em;
 `;
 
 const lst: Goal[] = [
@@ -24,45 +26,71 @@ const lst: Goal[] = [
         "name": "추가하는 목표",
         "startDate": "2022-10-21",
         "endDate": "2022-10-21",
-        "complete" : 5,
-        "incomplete":3
+        "complete": 5,
+        "incomplete": 3
     }, {
         "id": 2,
         "categoryId": 1,
         "name": "수정 목표222",
         "startDate": "2022-10-21",
         "endDate": "2022-11-21",
-        "complete" : 85,
-        "incomplete":73
+        "complete": 85,
+        "incomplete": 73
     }, {
         "id": 3,
         "categoryId": 2,
         "name": "추가하는 목표222",
         "startDate": "2022-10-21",
         "endDate": "2022-10-21",
-        "complete" : 52,
-        "incomplete":743
+        "complete": 52,
+        "incomplete": 743
     }
 ]
 
 function GoalList() {
+    const [isComplete, setIsComplete] = useState<boolean>(false)
 
-    const GoalElemList = lst.map(
-        (item) => <GoalElem
-            key={item.id}
-            id={item.id}
-            categoryId={item.categoryId}
-            name={item.name}
-            startDate={item.startDate}
-            endDate={item.endDate}
-            complete={item.complete}
-            incomplete={item.incomplete}
-            />
-    )
+    const GoalElemList = lst.map((item) => <GoalElem key = {
+        item.id
+    }
+    id = {
+        item.id
+    }
+    categoryId = {
+        item.categoryId
+    }
+    name = {
+        item.name
+    }
+    startDate = {
+        item.startDate
+    }
+    endDate = {
+        item.endDate
+    }
+    complete = {
+        item.complete
+    }
+    incomplete = {
+        item.incomplete
+    } />)
 
-    return (<GoalListBox>
-        {GoalElemList}
-    </GoalListBox>);
+    const changeList = (comp : boolean): void => {
+        setIsComplete(!comp)
+    }
+
+    return (<div> < GoalNav isComplete = {
+        isComplete
+    }
+    changeList = {
+        changeList
+    } /> {
+        isComplete
+            ? <div>aa</div>
+            : <GoalListBox> {
+                GoalElemList
+            }</GoalListBox>
+    }</div>);
 }
 
 export default GoalList;
